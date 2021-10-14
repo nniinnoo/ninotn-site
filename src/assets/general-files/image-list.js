ee930d786ed0596d9200223b5b9435fae09527c2
nniinnoo <nino.tannio@tuta.io>
2021-10-15 02:25:21 +0700

    add gallery setup

---
+++ src/assets/general-files/image-list.js
@@ -0,0 +1,9 @@
+const imgList = [
+  {},
+  {
+    caption: "Poto Tano Port, Sumbawa, Indonesia",
+    url: "https://drive.google.com/uc?id=1Hf7bFk2lfIs8AbP7il8IvIUcvMd1unLs",
+  },
+];
+
+export default imgList;
--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,18 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "@components/Layout";
-import { StaticImage } from "gatsby-plugin-image";
+import ArrowRightIcon from "@mui/icons-material/KeyboardArrowRightRounded";
+import ArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeftRounded";
+
+import imgList from "@assets/general-files/image-list";
 
 const Gallery = () => {
+  const imgTotal = imgList.length - 1;
+  const [imgIndex, setImgIndex] = useState(1);
+
+  const setPrevImg = () => {
+    if (imgIndex > 1) {
+      setImgIndex(imgIndex - 1);
+    }
+  };
+
+  const setNextImg = () => {
+    if (imgIndex < imgTotal) {
+      setImgIndex(imgIndex + 1);
+    }
+  };
+
   return (
     <Layout>
-      <center>
-        <StaticImage
-          alt="#"
-          src="../assets/moonlit-1878-by-ivan-aivazovsky.jpg"
-          width="720"
-        />
-        <h2>Under Construction :)</h2>
-      </center>
+      <div className="gallery__container">
+        <div className="gallery__content">
+          <img alt="#" quality={100} src={imgList[imgIndex].url} />
+          <div>
+            <figcaption>{imgList[imgIndex].caption}</figcaption>
+            <div className="gallery__skip">
+              <div
+                className={
+                  imgIndex > 1
+                    ? "gallery__prev-active"
+                    : "gallery__prev-inactive"
+                }
+                onClick={setPrevImg}
+                aria-hidden="true"
+              >
+                <ArrowLeftIcon />
+              </div>
+              <p>
+                {imgIndex} | {imgTotal}
+              </p>
+              <div
+                className={
+                  imgIndex < imgTotal
+                    ? "gallery__next-active"
+                    : "gallery__next-inactive"
+                }
+                onClick={setNextImg}
+                aria-hidden="true"
+              >
+                <ArrowRightIcon />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
