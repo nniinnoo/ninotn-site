33b0d49ee07c7860c3f55f7c6ec00ecee098a19e
nniinnoo <nino.tannio@tuta.io>
2021-10-15 23:16:56 +0700

    update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,36 +1,17 @@
 import * as React from "react";
-import { Link } from "gatsby";
-
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-};
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-};
-
-const paragraphStyles = {
-  marginBottom: 48,
-};
+import Layout from "@components/Layout";
+import blackhole from "@assets/blackhole.gif";
 
 // markup
 const NotFoundPage = () => (
-  <main style={pageStyles}>
-    <title>Not found</title>
-    <h1 style={headingStyles}>Page not found</h1>
-    <p style={paragraphStyles}>
-      Sorry{" "}
-      <span role="img" aria-label="Pensive emoji">
-        😔
-      </span>{" "}
-      we couldn’t find what you were looking for.
-      <Link to="/">Go home</Link>.
-    </p>
-  </main>
+  <Layout>
+    <center>
+      <h1>
+        <i>It looks like you're lost my lord..</i>
+      </h1>
+      <img style={{ borderRadius: "50%" }} src={blackhole} alt="" />
+    </center>
+  </Layout>
 );
 
 export default NotFoundPage;
