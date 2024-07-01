38c56de24095f2a01fb5665d5ff744877d9dc9b5
Nino Tannio <me@ninotn.com>
2024-07-01 14:06:13 +0200

    fix: still trying to fix jvectormap

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,7 @@ import React from "react";
 import Layout from "@components/Layout";
 import { StaticImage } from "gatsby-plugin-image";
 import "jvectormap-next/jquery-jvectormap.css";
+import { VectorMap } from "@react-jvectormap/core";
 
 import SEO from "@components/SEO";
 
